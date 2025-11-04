e27c10daf493e4874cb3af2164cfb724f181afcb
Jose Eliodoro <jose.santana@bsatech.io>
2025-11-04 17:46:40 -0300

    feat: implementa rota de conexão com o websocket

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,8 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, WebSocket
 from app.schemas import UserSchema, MessageGlobal, MessageCollection
 from app.db import user_collection, chats_collection
 from datetime import datetime
-
+from app.websocket_manager import websocket_manager
 router = APIRouter()
 
 def now():
@@ -21,12 +21,14 @@ async def create(user: UserSchema):
 
 @router.post("/geral-chat")
 async def geral_chat(msgDto: MessageGlobal):
-    print()
+
     if not await user_collection.find_one({"username": msgDto.username}):
         raise HTTPException(404, "usuário não encontrado")
+    
     new_msg = {"date": now(), **msgDto.model_dump(exclude=["id", "date"])}
     result = await chats_collection.insert_one(new_msg)
     new_msg["_id"] = f"{result.inserted_id}"
+
     return new_msg
 
 
@@ -34,3 +36,13 @@ async def geral_chat(msgDto: MessageGlobal):
 async def list_message():
     return MessageCollection(messages=await chats_collection.find().to_list(100))
 
+from uuid import uuid4
+
+@router.websocket("/ws")
+async def connect(websocket: WebSocket):
+    id = str(uuid4)
+    await websocket_manager.connect(websocket, id)
+    try:
+        await websocket_manager.send(id, {"hello": "world"})
+    except:
+        await websocket_manager.disconnect(id)
