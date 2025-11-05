e0b668f31664efc2c0424273e7112976d2f76b22
Jose Eliodoro <jose.santana@bsatech.io>
2025-11-05 15:18:11 -0300

    refactor: remove a rota de websocket do arquivo de routes e coloca na main

--- app/main.py
+++ app/main.py
@@ -1,6 +1,112 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, WebSocket, WebSocketDisconnect
+from uuid import uuid4
+
+from app.db import user_collection, chats_collection
+from app.websocket_manager import websocket_manager
+from app.schemas import MessageGlobal
 from app.routes import router
 
-app = FastAPI()
+from contextlib import asynccontextmanager
+from datetime import datetime
+import json
+import asyncio
+
+broadcast_task = None
+
+# função executada no inicio do ciclo de vida da aplicação e no final
+@asynccontextmanager
+async def lifespan(app):
+  
+  global broadcast_task
+
+  # antes do app iniciar
+  broadcast_task = asyncio.create_task(websocket_manager.subscribes_channel())
+
+  yield 
+  broadcast_task.cancel()
+  await websocket_manager.disconnect_all()
+
+
+app = FastAPI(lifespan=lifespan)
 app.include_router(router)
- 
+
+
+def now():
+    return datetime.now().timestamp()
+
+async def save_message_geral(msgDto: MessageGlobal):
+
+    if not await user_collection.find_one({"username": msgDto.username}):
+        raise HTTPException(404, "usuário não encontrado")
+    
+    new_msg = {"date": now(), **msgDto.model_dump(exclude=["_id", "date", "username_receive"], exclude_none=True)}
+    result = await chats_collection.insert_one(new_msg)
+    new_msg["_id"] = f"{result.inserted_id}"
+    
+    return new_msg
+
+async def save_message_private(msgDto: MessageGlobal):
+
+    if not await user_collection.find_one({"username": msgDto.username}):
+        raise HTTPException(404, "usuário não encontrado")
+    if not await user_collection.find_one({"username": msgDto.username_receive}):
+        raise HTTPException(404, "destinatário não encontrado")
+    
+    new_msg = {"date": now(), **msgDto.model_dump(exclude=["_id", "date"], exclude_none=True)}
+    result = await chats_collection.insert_one(new_msg)
+    new_msg["_id"] = f"{result.inserted_id}"
+    
+    return new_msg
+
+@app.websocket("/ws")
+async def connect(websocket: WebSocket):
+    socket_id = str(uuid4())
+    username = websocket.query_params.get('username')
+    try:
+        await websocket_manager.connect(websocket, socket_id, username)
+        await websocket_manager.send(socket_id, {"code": 200, "details": "conexão iniciada com sucesso"})
+    
+        while True:
+            data: str = await websocket.receive_text()
+            data_json: dict = json.loads(data)
+
+            if not data_json.get('chat') or not data_json.get("msg"):
+                await websocket_manager.send(socket_id, {"code": 400, "error": "requisição inválida"})
+            
+            chat = data_json['chat']
+            msg = data_json["msg"]
+
+            if(chat == "geral"):
+                try:
+                    message = await save_message_geral(MessageGlobal(username=username, msg=msg, chat=chat))
+                    await websocket_manager.pub_message(
+                        channel="geral", 
+                        socket_id=socket_id,
+                        msg=message
+                    )
+                except HTTPException as e:
+                    await websocket_manager.send(socket_id, {"code": 400, "error": e.detail})
+                    
+            elif(chat == "private"):
+                if not data_json.get("username_receive"):
+                    await websocket_manager.send(socket_id, {"code": 400, "error": "requisição inválida"})
+                try:
+                    message = await save_message_private(MessageGlobal(
+                        username=username, 
+                        msg=msg, chat=chat, 
+                        username_receive=data_json["username_receive"]
+                    ))
+                    await websocket_manager.pub_message(
+                        channel="private",
+                        socket_id=socket_id,
+                        msg=message
+                    )
+                except HTTPException as e:
+                    await websocket_manager.send(socket_id, {"code": 400, "error": e.detail})
+
+    except WebSocketDisconnect:
+        await websocket_manager.disconnect(socket_id)
+    
+    except Exception as e:
+        return HTTPException(400, e)
+    
--- app/routes.py
+++ app/routes.py
@@ -1,34 +1,8 @@
-from fastapi import APIRouter, HTTPException, WebSocket, WebSocketDisconnect
-from app.schemas import UserSchema, MessageGlobal, MessageCollection, UserCollection
+from fastapi import APIRouter, HTTPException
+from app.schemas import UserSchema, MessageCollection, UserCollection
 from app.db import user_collection, chats_collection
-from datetime import datetime
-from app.websocket_manager import websocket_manager
-from uuid import uuid4
-import json
-import asyncio
 
-
-from contextlib import asynccontextmanager
-
-broadcast_task = None
-
-# função executada no inicio do ciclo de vida da aplicação e no final
-@asynccontextmanager
-async def lifespan(app):
-  
-  global broadcast_task
-
-  # antes do app iniciar
-  broadcast_task = asyncio.create_task(websocket_manager.subscribes_channel())
-
-  yield 
-  broadcast_task.cancel()
-  await websocket_manager.disconnect_all()
-
-router = APIRouter(lifespan=lifespan)
-
-def now():
-    return datetime.now().timestamp()
+router = APIRouter()
 
 @router.post("/create-user")
 async def create(user: UserSchema):
@@ -40,30 +14,6 @@ async def create(user: UserSchema):
     new_user["_id"] = f"{result.inserted_id}"
 
     return new_user
-
-async def save_message_geral(msgDto: MessageGlobal):
-
-    if not await user_collection.find_one({"username": msgDto.username}):
-        raise HTTPException(404, "usuário não encontrado")
-    
-    new_msg = {"date": now(), **msgDto.model_dump(exclude=["_id", "date", "username_receive"], exclude_none=True)}
-    result = await chats_collection.insert_one(new_msg)
-    new_msg["_id"] = f"{result.inserted_id}"
-    
-    return new_msg
-
-async def save_message_private(msgDto: MessageGlobal):
-
-    if not await user_collection.find_one({"username": msgDto.username}):
-        raise HTTPException(404, "usuário não encontrado")
-    if not await user_collection.find_one({"username": msgDto.username_receive}):
-        raise HTTPException(404, "destinatário não encontrado")
-    
-    new_msg = {"date": now(), **msgDto.model_dump(exclude=["_id", "date"], exclude_none=True)}
-    result = await chats_collection.insert_one(new_msg)
-    new_msg["_id"] = f"{result.inserted_id}"
-    
-    return new_msg
 
 @router.delete("/delete-all-message")
 async def delete_all_message():
@@ -78,54 +28,5 @@ async def list_message():
 async def list_message():
     return UserCollection(messages=await user_collection.find().to_list(100))
 
-@router.websocket("/ws")
-async def connect(websocket: WebSocket):
-    socket_id = str(uuid4())
-    username = websocket.query_params.get('username')
-    try:
-        await websocket_manager.connect(websocket, socket_id, username)
-        await websocket_manager.send(socket_id, {"code": 200, "details": "conexão iniciada com sucesso"})
-    
-        while True:
-            data: str = await websocket.receive_text()
-            data_json: dict = json.loads(data)
 
-            if not data_json.get('chat') or not data_json.get("msg"):
-                await websocket_manager.send(socket_id, {"code": 400, "error": "requisição inválida"})
-            
-            chat = data_json['chat']
-            msg = data_json["msg"]
 
-            if(chat == "geral"):
-                try:
-                    message = await save_message_geral(MessageGlobal(username=username, msg=msg, chat=chat))
-                    await websocket_manager.pub_message(
-                        channel="geral", 
-                        socket_id=socket_id,
-                        msg=message
-                    )
-                except HTTPException as e:
-                    await websocket_manager.send(socket_id, {"code": 400, "error": e.detail})
-                    
-            elif(chat == "private"):
-                if not data_json.get("username_receive"):
-                    await websocket_manager.send(socket_id, {"code": 400, "error": "requisição inválida"})
-                try:
-                    message = await save_message_private(MessageGlobal(
-                        username=username, 
-                        msg=msg, chat=chat, 
-                        username_receive=data_json["username_receive"]
-                    ))
-                    await websocket_manager.pub_message(
-                        channel="private",
-                        socket_id=socket_id,
-                        msg=message
-                    )
-                except HTTPException as e:
-                    await websocket_manager.send(socket_id, {"code": 400, "error": e.detail})
-
-    except WebSocketDisconnect:
-        await websocket_manager.disconnect(socket_id)
-    
-    except Exception as e:
-        return HTTPException(400, e)
