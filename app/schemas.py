9d47a6346f6b3f0a09b5e8f6daf9c44a16e83730
Jose Eliodoro <jose.santana@bsatech.io>
2025-11-05 10:06:42 -0300

    feat: valida campo de datetime e cria colecão de usuários

--- app/schemas.py
+++ app/schemas.py
@@ -1,17 +1,27 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, field_validator
 from typing import List
 from datetime import datetime
 
 class UserSchema(BaseModel):
-    id: str | None = None
+    _id: str | None = None
     username: str
 
 class MessageGlobal(BaseModel):
-    id: str | None = None
-    date: datetime | None = None
-    username: str
     msg: str
+    username: str
+    _id: str | None = None
+    date: datetime | None = None
+    chat: str | None = None
+    username_receive: str | None = None
+    
+    @field_validator("date", mode="before")
+    def convert_str_to_datetime(cls, value):
+        if isinstance(value, str):
+            return datetime.strptime(value, "%d/%m/%Y %H:%M:%S")
+        return value
 
-
+class UserCollection(BaseModel):
+    messages: List[UserSchema]
+    
 class MessageCollection(BaseModel):
     messages: List[MessageGlobal]
