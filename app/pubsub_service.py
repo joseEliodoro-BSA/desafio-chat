149a876e18b642397451386b5b8a4610a627245c
José Carlos <jose.santana@bsatech.io>
2025-11-12 15:24:57 -0300

    feat: classe PubSub definida como Singleton

--- app/pubsub_service.py
+++ app/pubsub_service.py
@@ -4,22 +4,29 @@ import json
 import os
 import asyncio
 from threading import Thread
+from app.singleton import Singleton
 
-class PubSub:
+
+class PubSub(metaclass=Singleton):
     def __init__(self):
         self.redis_client = Redis(
-            host="redis", 
+            # host="redis", 
+            host="localhost", 
             port=6379, 
             encoding="utf-8", 
             decode_responses=True
         )
 
-    def sub(self, room: str, fn: Callable):
+    def sub(self, room: str, fn: Callable | None = None):
         pubsub = self.redis_client.pubsub()
         pubsub.subscribe(room)
         for msg in pubsub.listen():
             if msg and msg["type"] == "message":
-                asyncio.run(fn(msg))
+                if fn:
+                    asyncio.run(fn(msg))
+                else:
+                    print(msg["data"])
+                
 
     def pub(self, room: str, msg: Dict):
         self.redis_client.publish(room, json.dumps(msg))
@@ -28,14 +35,18 @@ async def main():
     pubsub = PubSub()
 
     # Rodar subscribers em threads para não travar o event loop
-    Thread(target=pubsub.sub, args=("test_1",), daemon=True).start()
-    Thread(target=pubsub.sub, args=("test_2",), daemon=True).start()
+    t1 = Thread(target=pubsub.sub, args=("test_1",), daemon=True)
+    # t2 = Thread(target=pubsub.sub, args=("test_2",), daemon=True)
+
+    t1.start()
+    # t2.start()
 
     while True:
         await asyncio.sleep(1)
 
 
 if __name__ == "__main__":
+    os.system("clear")
     try:
         asyncio.run(main())
     except KeyboardInterrupt:
