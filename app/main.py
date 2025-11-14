9f95e28dfdf7b3b5e25c17e1fc4149226ff7e115
José Carlos <jose.santana@bsatech.io>
2025-11-14 15:23:43 -0300

    feat: atualiza banco sobre status de login dos usuários

--- app/main.py
+++ app/main.py
@@ -1,18 +1,27 @@
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 from uuid import uuid4
 
-from app.services.websocket_service import WebSocketService
+from app.services import WebSocketService
 from app.routes import router
 
 from app.logger_config import setup_logging
+from contextlib import asynccontextmanager
+from app.db import db
 
 setup_logging()
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await db.users.update_many({}, {"$set": {"in_chat":False, "online":False}})
+    await db.chats.update_many({}, {"$set": {"users": []}})
+
+    yield
+    pass
+app = FastAPI(lifespan=lifespan)
 app.include_router(router)
 
 
-@app.websocket("/ws/{room}")
+@app.websocket("/ws")
 async def connect(websocket: WebSocket):
     try:
 
